ee5dde42f0f313261904b6ad74428aee63997201
Derek Gonzalez <gonzalezaguilar.derek@gmail.com>
2022-10-30 23:22:56 -0600

    PROYECTO ACABADO

--- ProyectoAutomatas.java
+++ ProyectoAutomatas.java
@@ -3,20 +3,21 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.StringTokenizer;
-
+import java.util.concurrent.BlockingQueue;
+import java.JOptionPane;
 
 public class ProyectoAutomatas {
-    static int palabrasTotales;
-    static int lineasTotales;
+    public static int palabrasTotales;
+    public static int lineasTotales;
     
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         // VARIABLE PARA LLEVAR EL CONTADOR DE PALABRAS
         palabrasTotales = 0;
         
         // VARIABLE PARA LLEVAR EL CONTADOR DE LINEAS
         lineasTotales = 0;
         
-        // OBJETO ARCHIVO CON LA RUTA DEL ARCHIVO A LEER
+        // LLAMAMOS AL METODO Y LE DAMOS EL ARCHIVO 
         File archivo = new File("C:\\Users\\gonza\\OneDrive\\Documentos\\GitHub\\Java\\TXT\\prueba1.txt");
         
         // LLAMADA AL METODO PARA CONTAR LAS PALABRAS Y LAS LINEAS
@@ -37,16 +38,17 @@ public class ProyectoAutomatas {
                 while((lineaLeida = archivoLeer.readLine()) != null) {
                     System.out.println(lineaLeida);
                     
-                    // CONTAMOS LAS PALABRAS DE LA LINEA
+                    // CONTAMOS LAS PALABRAS DE LA LINEA DIVIDE EL STRING EN SEGMENTOS O TOKENS Y SE LEE POR CARACTER 
                     StringTokenizer st = new StringTokenizer(lineaLeida);
                     
                     // AUMENTA EL CONTADOR DE LINEAS Y DE PALABRAS
                     lineasTotales = lineasTotales + 1;
                     palabrasTotales = palabrasTotales + st.countTokens();
                 }
-                System.out.println("");
+                
                 System.out.println("Numero de palabras: " + palabrasTotales);
                 System.out.println("Lineas totales: " + lineasTotales);
+                javax.swing.JOptionPane.showMessageDialog(null, "\nPalabras Totales: "+palabrasTotales+"\nLineas totales: "+lineasTotales);
                 
                 // CIERRA LA LECTURA
                 archivoLeer.close();
